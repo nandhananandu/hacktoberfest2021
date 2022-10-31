e3e1cca4a6b7ce00936f6e2e14116896222a742f
nandhananandu <117092430+nandhananandu@users.noreply.github.com>
2022-10-31 19:22:08 +0530

    Create vowelsConsonants.c

---
+++ vowelsConsonants.c
@@ -0,0 +1,8 @@
+/*C Program to count vowels, consonants, digits, special characters in given string*/
+#include <stdio.h>
+int main()
+{
+    char str[200];                                    //character array
+    int i, vowels, consonants, digits, space,special;  
+
+    vowels =  consonants = digits =  space= special = 0;
